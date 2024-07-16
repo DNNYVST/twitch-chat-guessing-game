bb89f18ab49f2f641c3dd9b5434ab8b6870831ad
Danny Vasta <djv711@gmail.com>
2024-07-16 18:37:02 -0400

    prettier

--- src/app/chat/components/user-message.tsx
+++ src/app/chat/components/user-message.tsx
@@ -1,13 +1,14 @@
 export interface Message {
-    name: string;
-    color: string;
-    message: string;
-};
+  name: string;
+  color: string;
+  message: string;
+}
 
-const UserMessage = ({name, color, message}: Message) => (
-    <div className="inline-block text-sm font-medium">
-        <strong style={{color: `${color}`}}>{name}</strong><span className="text-[#efeff1]">: {message}</span>
-    </div>
+const UserMessage = ({ name, color, message }: Message) => (
+  <div className="inline-block text-sm font-medium">
+    <strong style={{ color: `${color}` }}>{name}</strong>
+    <span className="text-[#efeff1]">: {message}</span>
+  </div>
 );
 
 export default UserMessage;

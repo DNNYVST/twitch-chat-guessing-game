2bf718718a85abc3a174573b4bc208acb366e9e6
Danny Vasta <djv711@gmail.com>
2024-07-27 18:31:46 -0400

    test creating generic grid/column setup

---
+++ src/app/components/core/styled/grid/grid.styled.tsx
@@ -0,0 +1,24 @@
+import styled, { css } from "styled-components";
+
+export const Grid = styled.div<{ gap?: string }>`
+  display: grid;
+
+  grid-template-columns: repeat(12, 1fr);
+  ${({ gap }) =>
+    gap &&
+    css`
+      grid-gap: ${gap};
+    `}
+`;
+
+Grid.defaultProps = {
+  gap: "0.5rem",
+};
+
+export const Column = styled.div<{ size: number }>`
+  border: 2px solid red;
+  ${({ size }) =>
+    css`
+      grid-column: span ${size};
+    `}
+`;
--- src/app/flex-playground/page.tsx
+++ src/app/flex-playground/page.tsx
@@ -1,48 +1,47 @@
 "use client";
 
-import styled, { css } from "styled-components";
-import {Button} from "../components/core/styled/button";
+import { Grid, Column } from "../components/core/styled/grid";
 
-const Container = styled.div`
-  display: grid;
+// const Container = styled.div`
+//   display: grid;
 
-  grid-gap: 0.5rem;
-  grid-template-rows: 1fr 2fr;
-  grid-template-areas:
-    "setup setup setup leaderboard"
-    "chat chat chat leaderboard";
-  padding: 0.5rem;
+//   grid-gap: 0.5rem;
+//   grid-template-rows: 1fr 2fr;
+//   grid-template-areas:
+//     "setup setup setup leaderboard"
+//     "chat chat chat leaderboard";
+//   padding: 0.5rem;
 
-  @media (max-width: 640px) {
-    grid-template-areas:
-      "setup"
-      "chat"
-      "leaderboard";
-  }
+//   @media (max-width: 640px) {
+//     grid-template-areas:
+//       "setup"
+//       "chat"
+//       "leaderboard";
+//   }
 
-  height: 80vh;
-  border: 2px solid red;
-    margin: 5% 5%;
+//   height: 80vh;
+//   border: 2px solid red;
+//   margin: 5% 5%;
 
-  @media (min-width: 640px) {
-    margin: 5% 20%;
-  }
-`;
+//   @media (min-width: 640px) {
+//     margin: 5% 20%;
+//   }
+// `;
 
-const Setup = styled.div`
-  grid-area: setup;
-  border: 2px solid purple;
-`;
+// const Setup = styled.div`
+//   grid-area: setup;
+//   border: 2px solid purple;
+// `;
 
-const Chat = styled.div`
-  grid-area: chat;
-  border: 2px solid green;
-`;
+// const Chat = styled.div`
+//   grid-area: chat;
+//   border: 2px solid green;
+// `;
 
-const Leaderboard = styled.div`
-  grid-area: leaderboard;
-  border: 2px solid blue;
-`;
+// const Leaderboard = styled.div`
+//   grid-area: leaderboard;
+//   border: 2px solid blue;
+// `;
 
 {
   /* <div className="flex flex-row items-stretch space-x-4 border-2 border-red-600 my-[5%] mx-[5%] sm:mx-[20%]"> */
@@ -50,15 +49,10 @@ const Leaderboard = styled.div`
 
 export default function Page() {
   return (
-    <Container>
-      <Setup>setup</Setup>
-      <Chat>chat</Chat>
-      <Leaderboard>leaderboard</Leaderboard>
-      {/* <div className="flex flex-col w-1/2 space-y-4 border-2 border-green-500">
-        <div className="p-4 border-2 border-purple-500 h-3/6">Projects</div>
-        <div className="p-4 border-2 border-purple-500 h-3/6">Empty </div>
-      </div>
-      <div className="w-1/2 p-6 border-2 border-purple-500 ">Study/Work</div> */}
-    </Container>
+    <Grid>
+      <Column size={12}>setup</Column>
+      <Column size={8}>chat</Column>
+      <Column size={4}>leaderboard</Column>
+    </Grid>
   );
 }
